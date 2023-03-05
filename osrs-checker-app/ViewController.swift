e4493ddc03be1ecc72e133b2fc8f846a5ba15006
matela <jmatelaneto@gmail.com>
2023-03-05 03:28:07 -0300

    added .gitignore

---
+++ osrs-checker-app/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  osrs-checker-app
+//
+//  Created by José Matela Neto on 05/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
